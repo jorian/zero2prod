b1c26fa0c6b1166e8bc1f063a068e39d0700f41c
jorian <10341677+jorian@users.noreply.github.com>
2022-08-10 08:54:06 +0200

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-fn main() {
-    println!("Hello, world!");
-    println!("Let's see now");
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    zero2prod::run(TcpListener::bind("127.0.0.1:0")?)?.await
 }
