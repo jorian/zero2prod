bd2c2becce4db1acbfb44000946ea1c1f11bb306
jorian <10341677+jorian@users.noreply.github.com>
2022-08-18 08:39:02 +0200

    db fix

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,10 @@ use zero2prod::configuration::get_configuration;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let config = get_configuration().expect("failed to read configuration");
-    let address = format!(
-        "{}:{}",
-        config.application.host, config.application.port
-    );
+    let address = format!("{}:{}", config.application.host, config.application.port);
     let listener = TcpListener::bind(&address)?;
 
-    let connection_pool = PgPool::connect(&config.database.connection_string())
-        .await
+    let connection_pool = PgPool::connect_lazy(&config.database.connection_string())
         .expect("Failed to connect to postgres");
 
     zero2prod::run(connection_pool, listener)?.await
